41d8aa042c9dacbd8c7b36db9008025c0776941c
CarlosAndresMarin <cmarininstrumentista@gmail.com>
2022-10-25 06:31:29 -0500

    Creacion generar y cifrar clave

--- src/services/autenticacion.service.ts
+++ src/services/autenticacion.service.ts
@@ -1,10 +1,22 @@
-import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+const cryptoJs = require('crypto-js');
+const passwordGen = require('password-generator');
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class AutenticacionService {
-  constructor(/* Add @inject to inject parameters */) {}
+  constructor(/* Add @inject to inject parameters */) { }
 
-  /*
-   * Add service methods here
-   */
+  cifrarClave(clave: string): string {
+    let claveCifrada = cryptoJs.MD5(clave).toString();
+
+    return claveCifrada
+  }
+  generarClave() {
+    let clave = passwordGen(8, false);
+    return clave();
+  }
+
+
+  //const  cifrarClave2 = (clave: string) => cryptoJs.MD5(clave).toString().toString();
+
 }
